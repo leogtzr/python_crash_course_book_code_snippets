6171d266c65e2ceea7cc1c46bc70d1364ab3658b
leogtzr <leogutierrezramirez@gmail.com>
2020-10-11 15:55:19 -0600

    add code about constants and multiple assignment

---
+++ constants1.py
@@ -0,0 +1,4 @@
+# Python doesn’t have built-in constant types, but Python programmers use all capital
+# letters to indicate a variable should be treated as a constant and never be changed
+MAX_VALUE = 234
+print(MAX_VALUE)
