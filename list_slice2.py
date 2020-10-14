48a09d5097bb7644448abc3856a948c9aaa63af8
leogtzr <leogutierrezramirez@gmail.com>
2020-10-13 21:51:22 -0600

    add more about slices

---
+++ list_slice2.py
@@ -0,0 +1,13 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print(players[:len(players)])
+# 2 en delante:
+print(players[2:len(players)])
+
+print(players[1:4])
+
+print(players[:3])
+
+print("~~~~~~~~~~~~~~~~~~~~~")
+# Getting the last three players:
+print(players[-3:])
