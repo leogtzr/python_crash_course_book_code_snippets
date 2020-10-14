5d1892b5b846d121912446ac5e3b0114649e73cb
leogtzr <leogutierrezramirez@gmail.com>
2020-10-13 22:27:09 -0600

    more about tuples

---
+++ looping_a_tuple1.py
@@ -0,0 +1,4 @@
+dimensions = (200, 50)
+
+for d in dimensions:
+    print(d)
---
+++ writing_over_a_tuple1.py
@@ -0,0 +1,11 @@
+point = (2, 4)
+print(point[0])
+print(point[1])
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~>")
+
+point = (2, 7, 3)
+
+print(point[0])
+print(point[1])
+print(point[2])
