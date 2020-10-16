0594464baa59e5685ed3b4ea6f18ac704dcafa5c
leogtzr <leogutierrezramirez@gmail.com>
2020-10-16 14:11:08 -0600

    add default values code example

---
+++ default_values1.py
@@ -0,0 +1,10 @@
+def describe_pet(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+def myf(name='Leo'):
+    print(f"Hello, {name.title()}")
+
+describe_pet(pet_name='Willie')
+myf('Leonardo')
