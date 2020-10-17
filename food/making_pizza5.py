d014665d71a6f7060340cbb25724de6837c8b3a3
leogtzr <leogutierrezramirez@gmail.com>
2020-10-17 10:44:43 -0600

    importing all functions in a module

---
+++ food/making_pizza5.py
@@ -0,0 +1,11 @@
+from pizza import *
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# you simply write import followed by the name of the module, makes every function
+# from the module available in your program. If you use this kind of import statement
+# to import an entire module named module_name.py, each function in the module is
+# available through the following syntax:
+
+#       module_name.function_name()
