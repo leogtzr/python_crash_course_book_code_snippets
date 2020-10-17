ac431de5a6654f818ea1faea23c148b89057d6ab
leogtzr <leogutierrezramirez@gmail.com>
2020-10-17 11:12:08 -0600

    some code about inheritance and overriding

---
+++ classes/inheritance/electric_car_override.py
@@ -0,0 +1,49 @@
+class Car:
+    """A simple attempt to represent a car."""
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+
+class ElectricCar(Car):
+    """Represent aspects of a car, specific to electric vehicles."""
+
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        # It's own attributes and using a default value.
+        self.battery_size = 75
+
+    def describe_battery(self):
+        """Print a statement describing the battery size."""
+        print(f"This car has a {self.battery_size}-kWh battery.")
+
+
+my_tesla = ElectricCar('tesla', 'model s', 2019)
+print(my_tesla.get_descriptive_name())
+
+# Notes:
+
+# The super() function at ➍ is a special function that allows you to call a method
+# from the parent class. This line tells Python to call the __init__() method from Car,
+# which gives an ElectricCar instance all the attributes defined in that method.
+# The name super comes from a convention of calling the parent class a superclass and
+# the child class a subclass.
