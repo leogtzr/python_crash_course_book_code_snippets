57470f14afc041acd4f6d099ed4aa6e7cd91fb80
leogtzr <leogutierrezramirez@gmail.com>
2020-10-18 17:13:03 -0600

    first unit test

---
+++ testing_code/name_function1.py
@@ -0,0 +1,4 @@
+def get_formatted_name(first, last):
+    """ Generate a neatly formatted full name """
+    full_name = f"{first.title()} {last.title()}"
+    return full_name
---
+++ testing_code/test_name.py
@@ -0,0 +1,17 @@
+import unittest
+#from name_function1 import get_formatted_name
+import name_function1
+
+
+class NamesTestCase(unittest.TestCase):
+    """Tests for 'name_function.py'."""
+
+    def test_first_last_name(self):
+        """Do names like 'Janis Joplin' work?"""
+
+        formatted_name = name_function1.get_formatted_name('janis', 'joplin')
+        self.assertEqual(formatted_name, 'Janis Joplin')
+
+
+if __name__ == '__main__':
+    unittest.main()
