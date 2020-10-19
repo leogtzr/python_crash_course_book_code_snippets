62ce3a7be53ab91c7912135d464eeebef45bd481
leogtzr <leogutierrezramirez@gmail.com>
2020-10-19 16:01:32 -0600

    moving the ship, left and right

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """ Start the main loop for the game. """
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -33,7 +34,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     # Move the ship to the right:
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
