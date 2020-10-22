0afa586fccd3323d7da00c527e4abf4d35bd1bbc
leogtzr <leogutierrezramirez@gmail.com>
2020-10-21 18:02:28 -0600

    drawing an alien

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +24,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
         # Set the background color:
         self.bg_color = (230, 230, 230)
 
@@ -60,6 +64,8 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
 
+        self.aliens.draw(self.screen)
+
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
@@ -82,9 +88,16 @@ class AlienInvasion:
             self.ship.moving_left = False
 
     def _fire_bullet(self):
-        """ Create a new bullet and add it to the bullets group """
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            """ Create a new bullet and add it to the bullets group """
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _create_fleet(self):
+        """ Create the fleet of aliens. """
+        # Make an alien:
+        alien = Alien(self)
+        self.aliens.add(alien)
 
 
 if __name__ == '__main__':
